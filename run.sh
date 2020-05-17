19a4bfd54a6f5b644ff88814648a05625d252821
LI <technik@ingold.li>
2020-05-17 14:58:52 +0200

    added sql to run.sh

--- run.sh
+++ run.sh
@@ -28,8 +28,12 @@ function pause(){
 ./covid-push2hdfs.sh
 
 ## ##################################################
-## ddl
-# hive_cli -f create.sql
+## create DB & Tables
+hive_cli -f create.sql
+pause 'Press [Enter] key to continue: load data into tables'
 
-
+## ##################################################
+## load data into tables
+hive_cli -f load_data.sql
+pause 'Press [Enter] key to continue: end script'
 
