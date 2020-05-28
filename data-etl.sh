18d14065703bd69f96d435e94819ccc272f66921
LI <technik@ingold.li>
2020-05-28 16:38:31 +0200

    change format of datetime

--- data-etl.sh
+++ data-etl.sh
@@ -15,13 +15,13 @@ function pause(){
 ## Change Format of TXT for correct datetime
 echo "Change CHFJPY.txt"
 cd stock/
-cat CHFJPY2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2" "$3","$4","$5","$6","$7}' > CHFJPY.txt
+cat CHFJPY2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2","$4","$5","$6","$7}' > CHFJPY.txt
 pause 'Press [Enter] key to continue: change EURCHF'
-cat EURCHF2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2" "$3","$4","$5","$6","$7}' > EURCHF.txt
+cat EURCHF2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2","$4","$5","$6","$7}' > EURCHF.txt
 pause 'Press [Enter] key to continue: change GBPCHF'
-cat GBPCHF2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2" "$3","$4","$5","$6","$7}' > GBPCHF.txt
+cat GBPCHF2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2","$4","$5","$6","$7}' > GBPCHF.txt
 pause 'Press [Enter] key to continue: change USDCHF'
-cat USDCHF2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2" "$3","$4","$5","$6","$7}' > USDCHF.txt
+cat USDCHF2.txt | sed 's/./&-/11;s/./&-/14;s/./&:/20;s/./&:/23' | awk -F, '{print $1","$2","$4","$5","$6","$7}' > USDCHF.txt
 
 pause 'Press [Enter] key to continue: change file permissions'
 
@@ -33,4 +33,3 @@ chmod 755 GBPCHF.txt
 chmod 755 USDCHF.txt
 
 pause 'Press [Enter] key to continue: push data to hdfs'
-
