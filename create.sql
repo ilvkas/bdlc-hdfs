e0250d5a8ad2ec2cfc27807b0fff155c744274e4
LI <technik@ingold.li>
2020-05-29 23:26:15 +0200

    added files for hive table

--- create.sql
+++ create.sql
@@ -7,10 +7,38 @@ show tables;
 
 -- --------------------------------------------------
 -- tabula rasa for src_* tables
+DROP TABLE IF EXISTS bdlc_data_pro.src_chfjpy_txt;
+DROP TABLE IF EXISTS bdlc_data_pro.src_eurchf_txt;
+DROP TABLE IF EXISTS bdlc_data_pro.src_gbpchf_txt;
+DROP TABLE IF EXISTS bdlc_data_pro.src_usdchf_txt;
 DROP TABLE IF EXISTS bdlc_data_pro.covid_19_json;
 
 -- --------------------------------------------------
 -- DDL src_* Tables
+CREATE TABLE src_chfjpy_txt (Ticker STRING,Datum TIMESTAMP,Open FLOAT,High FLOAT,Low FLOAT,Close FLOAT)
+COMMENT 'CHF-JPY Market'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
+
+CREATE TABLE src_eurchf_txt (Ticker STRING,Datum TIMESTAMP,Open FLOAT,High FLOAT,Low FLOAT,Close FLOAT)
+COMMENT 'EUR-CHF Market'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
+
+CREATE TABLE src_gbpchf_txt (Ticker STRING,Datum TIMESTAMP,Open FLOAT,High FLOAT,Low FLOAT,Close FLOAT)
+COMMENT 'GBP-CHF Market'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
+
+CREATE TABLE src_usdchf_txt (Ticker STRING,Datum TIMESTAMP,Open FLOAT,High FLOAT,Low FLOAT,Close FLOAT)
+COMMENT 'USD-CHF Market'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
+
 CREATE TABLE IF NOT EXISTS covid_19_json (data STRING)
 COMMENT 'COVID 19 JSON DATA'
 STORED AS TEXTFILE;
--- load_data.sql
+++ load_data.sql
@@ -5,6 +5,10 @@ use bdlc_data_pro;
 -- --------------------------------------------------
 -- LOAD Data into tables
 load data inpath '/user/zeppelin/pro/covid_data.json' into table covid_19_json;
+load data inpath '/user/zeppelin/pro/USDCHF.txt' into table src_usdchf_txt;
+load data inpath '/user/zeppelin/pro/GBPCHF.txt' into table src_gbpchf_txt;
+load data inpath '/user/zeppelin/pro/EURCHF.txt' into table src_eurchf_txt;
+load data inpath '/user/zeppelin/pro/CHFJPY.txt' into table src_chfjpy_txt;
 
 -- --------------------------------------------------
 -- CREATE VIEW
@@ -23,3 +27,17 @@ get_json_object(data, '$.countryterritoryCode') AS LandCode,
 get_json_object(data, '$.popData2018') AS Population,
 get_json_object(data, '$.continentExp') AS Kontinent
 FROM covid_19_json;
+
+-- --------------------------------------------------
+-- show infos
+-- show tables;
+-- 
+-- DESCRIBE covid_data;  
+-- DESCRIBE covid_test;
+-- DESCRIBE src_chfjpy_txt;
+-- DESCRIBE src_eurchf_txt;  
+-- DESCRIBE src_gbpchf_txt;  
+-- DESCRIBE src_usdchf_txt;  
+-- 
+-- DESCRIBE FORMATTED measurements;  
+
