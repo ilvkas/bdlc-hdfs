f070d19872a705b7ddb82cc1e679caf56634439a
LI <technik@ingold.li>
2020-05-30 15:28:38 +0200

    added bz2 files

--- data-push2hdfs.sh
+++ data-push2hdfs.sh
@@ -16,9 +16,25 @@ function pause(){
 if ! hdfs dfs -test -d /user/zeppelin/pro/; then hdfs dfs -mkdir /user/zeppelin/pro/; fi
 
 ## ##################################################
+test -e stock/CHFJPY.txt.bz2 || bzip2 -c stock/CHFJPY.txt > stock/CHFJPY.txt.bz2
+chmod 755 stock/CHFJPY.txt.bz2
+test -e stock/EURCHF.txt.bz2 || bzip2 -c stock/EURCHF.txt > stock/EURCHF.txt.bz2
+chmod 755 stock/EURCHF.txt.bz2
+test -e stock/GBPCHF.txt.bz2 || bzip2 -c stock/GBPCHF.txt > stock/GBPCHF.txt.bz2
+chmod 755 stock/GBPCHF.txt.bz2
+test -e stock/USDCHF.txt.bz2 || bzip2 -c stock/USDCHF.txt > stock/USDCHF.txt.bz2
+chmod 755 stock/USDCHF.txt.bz2
+
+pause 'Press [Enter] key to continue: push final data to hdfs'
+
+## ##################################################
 ## push files to hdfs
 echo "pushing data to hdfs"
 hdfs dfs -put -f stock/CHFJPY.txt /user/zeppelin/pro/
+hdfs dfs -put -f stock/CHFJPY.txt.bz2 /user/zeppelin/pro/
 hdfs dfs -put -f stock/EURCHF.txt /user/zeppelin/pro/
+hdfs dfs -put -f stock/EURCHF.txt.bz2 /user/zeppelin/pro/
 hdfs dfs -put -f stock/GBPCHF.txt /user/zeppelin/pro/
+hdfs dfs -put -f stock/GBPCHF.txt.bz2 /user/zeppelin/pro/
 hdfs dfs -put -f stock/USDCHF.txt /user/zeppelin/pro/
+hdfs dfs -put -f stock/USDCHF.txt.bz2 /user/zeppelin/pro/
--- data-push2hdfs_end.sh
+++ data-push2hdfs_end.sh
@@ -18,10 +18,10 @@ if ! hdfs dfs -test -d /user/zeppelin/pro/; then hdfs dfs -mkdir /user/zeppelin/
 ## ##################################################
 ## push files to hdfs
 echo "pushing data to hdfs"
-hdfs dfs -put -f stock/CHFJPY.txt /user/zeppelin/pro/
-hdfs dfs -put -f stock/EURCHF.txt /user/zeppelin/pro/
-hdfs dfs -put -f stock/GBPCHF.txt /user/zeppelin/pro/
-hdfs dfs -put -f stock/USDCHF.txt /user/zeppelin/pro/
+hdfs dfs -put -f stock/CHFJPY.txt.bz2 /user/zeppelin/pro/
+hdfs dfs -put -f stock/EURCHF.txt.bz2 /user/zeppelin/pro/
+hdfs dfs -put -f stock/GBPCHF.txt.bz2 /user/zeppelin/pro/
+hdfs dfs -put -f stock/USDCHF.txt.bz2 /user/zeppelin/pro/
 cp ../../../zeppelin/covid_data_2.json covid_data.json
 chmod 755 covid_data.json
 hdfs dfs -put -f covid_data.json /user/zeppelin/pro/
